4ce4dcf7a1ee82c736686b0c2c625cf419eb09ce
Gromit-IT <165180593+Gromit-IT@users.noreply.github.com>
2025-03-17 18:57:30 +0900

    회원가입-1
    회원가입 기능 구현

--- src/main/java/com/swings/user/controller/UserController.java
+++ src/main/java/com/swings/user/controller/UserController.java
@@ -1,6 +1,25 @@
 package com.swings.user.controller;
 
+import com.swings.user.dto.UserDTO;
+import com.swings.user.entity.UserEntity;
+import com.swings.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> registerUser(@RequestBody UserDTO dto) {
+        UserEntity newUser = userService.registerUser(dto);
+        return ResponseEntity.ok("회원가입 성공! ID: " + newUser.getUserId());
+    }
 }
--- src/main/java/com/swings/user/service/UserService.java
+++ src/main/java/com/swings/user/service/UserService.java
@@ -1,4 +1,54 @@
 package com.swings.user.service;
 
+import com.swings.user.dto.UserDTO;
+import com.swings.user.entity.UserEntity;
+import com.swings.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    /**
+     * 회원가입 (비밀번호 암호화 후 저장)
+     */
+    public UserEntity registerUser(UserDTO dto) {
+        // 아이디 중복 확인
+        if (userRepository.findByUsername(dto.getUsername()).isPresent()) {
+            throw new IllegalArgumentException("이미 존재하는 아이디입니다.");
+        }
+        // 비밀번호 암호화
+        String encryptedPassword = passwordEncoder.encode(dto.getPassword());
+
+        // UserEntity 생성 및 저장
+        UserEntity user = UserEntity.builder()
+                .username(dto.getUsername())
+                .password(encryptedPassword)
+                .name(dto.getName())
+                .phonenumber(dto.getPhonenumber())
+                .job(dto.getJob())
+                .golfSkill(dto.getGolfSkill())
+                .mbti(dto.getMbti())
+                .hobbies(dto.getHobbies())
+                .religion(dto.getReligion())
+                .smoking(dto.getSmoking())
+                .drinking(dto.getDrinking())
+                .introduce(dto.getIntroduce())
+                .userImg(dto.getUserImg())
+                .role(dto.getRole())
+                .createdAt(LocalDateTime.now()) // 현재 시간 설정
+                .build();
+
+        return userRepository.save(user);
+
+
+
+    }
 }
