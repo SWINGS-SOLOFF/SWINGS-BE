f8e156ab5f3cd14e43d3cbfe8ef3935d30ae92f2
Gromit-IT <165180593+Gromit-IT@users.noreply.github.com>
2025-05-22 15:07:04 +0900

    리팩토링-2(이우찬)
    불필요 파일 삭제 예정

--- src/main/java/com/swings/security/PasswordEncoderUtil.java
+++ src/main/java/com/swings/security/PasswordEncoderUtil.java
@@ -1,12 +1,14 @@
-package com.swings.security;
+//package com.swings.security;
+//
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+//public class PasswordEncoderUtil {
+//    public static void main(String[] args) {
+//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//        String rawPassword = "1234";
+//        String encodedPassword = encoder.encode(rawPassword);
+//        System.out.println("암호화된 비밀번호: " + encodedPassword);
+//    }
+//
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-public class PasswordEncoderUtil {
-    public static void main(String[] args) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String rawPassword = "1234";
-        String encodedPassword = encoder.encode(rawPassword);
-        System.out.println("암호화된 비밀번호: " + encodedPassword);
-    }
-}
+//삭제 예정
